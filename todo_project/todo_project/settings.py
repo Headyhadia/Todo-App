a00784b1fe4e8ab2681b8ef84473209c211b7e72
Hadia <hadiaabq@gmail.com>
2025-12-01 16:57:18 +0500

    feat: Enhance Todo model and settings configuration; add .gitignore

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--gy15-4@^px#28fk%qa1z2wwt3x=sl75-h6rdnc9&9byhmw1^y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
